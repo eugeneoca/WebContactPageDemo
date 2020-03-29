60371ff162b22a5e636abec6a3dd3607a764dd5d
Eugene Oca <eugenebasiliscooca@gmail.com>
2020-03-29 12:29:03 +0800

    Add project files.

---
+++ WebContactPage/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebContactPage.Models;
+
+namespace WebContactPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendForm(Entry entryModel)
+        {
+            using (WebContactDBEntities db = new WebContactDBEntities())
+            {
+                db.Entries.Add(entryModel);
+                db.SaveChanges();
+            }
+            return Redirect("/contact");
+        }
+    }
+}
---
+++ WebContactPage/Controllers/RecordController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebContactPage.Models;
+
+namespace WebContactPage.Controllers
+{
+    public class RecordController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            using (WebContactDBEntities db = new WebContactDBEntities())
+            {
+                List<Entry> records = db.Entries.ToList();
+                return View(records);
+            }
+        }
+    }
+}
